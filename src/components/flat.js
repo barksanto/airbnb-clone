ed19e4427aab7edd8e7ff5993bb49ba8e5c074ac
Barkley Santo <barkley.santo@gmail.com>
2021-01-19 13:49:07 -0600

    created style constant that will contain dynamic backgroudn image url for each flat

--- src/components/flat.js
+++ src/components/flat.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import "./flat";
+
+
 
 class Flat extends Component {
   render() {
+    const title = this.props.flat.price
+      + this.props.flat.priceCurrency
+      + " - " + this.props.flat.name;
+
+    const style = {
+      backgroundImage: `url('${this.props.flat.imageUrl}')`
+    }
+
     return (
       <div className="flat">
-        <div className="flat-picture"></div>
-        <div className="flat-title"></div>
+        <div className="flat-picture" style={style}></div>
+        <div className="flat-title">
+          {this.props.title}
+        </div>
       </div>
     );
   }
